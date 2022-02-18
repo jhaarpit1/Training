3b5c95c1bacc1b5041d825c4f1826a9155c923af
Arpit Jha <arpit.jha@pkware.com>
2022-02-18 19:05:11 +0530

    Day 3-classes and objects

---
+++ src/src/day_3_classes_objects/Print_Circle_Area.java
@@ -0,0 +1,40 @@
+package src.day_3_classes_objects;
+
+import java.util.Scanner;
+
+public class Print_Circle_Area {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        String color = sc.nextLine();
+        int radius = sc.nextInt();
+        input_cirlce inp = new input_cirlce();
+        inp.input_cirlce(name,color,radius);
+        System.out.println("Area of circle named "+inp.getName()+" of "+inp.getColor()
+        +" color is of area "+radius*radius*3.14);
+    }
+}
+
+    class input_cirlce {
+    private String name;
+    private String color;
+    private int radius;
+
+    void input_cirlce(String name, String color, int radius){
+        this.name = name;
+        this.color = color;
+        this.radius = radius;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+}
